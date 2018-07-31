200a239ed60aadab1f185c8f430e2a435847c5d2
Michiel Post <michiel@q42.nl>
2018-07-31 23:13:56 +0200

    Added ColorloopWheelEffect

--- HueLightDJ.Effects/Layers/ColorloopEffect.cs
+++ HueLightDJ.Effects/Layers/ColorloopEffect.cs
@@ -21,17 +21,6 @@ namespace HueLightDJ.Effects.Layers
       Func<TimeSpan> customWaitTime = () => waitTime();
 
       layer.To2DGroup().SetRandomColor(cancellationToken, IteratorEffectMode.All, IteratorEffectMode.All, waitTime, waitTime, null);
-
-      //var r = new Random();
-
-      //while (!cancellationToken.IsCancellationRequested)
-      //{
-      //  var randomColor = new RGBColor(r.NextDouble(), r.NextDouble(), r.NextDouble());
-
-
-      //  await Task.Delay(waitTime());
-      //}
-
     }
   }
 }
---
+++ HueLightDJ.Effects/Layers/ColorloopWheelEffect.cs
@@ -0,0 +1,42 @@
+using HueLightDJ.Effects.Base;
+using Q42.HueApi.ColorConverters;
+using Q42.HueApi.Streaming.Effects;
+using Q42.HueApi.Streaming.Effects.BasEffects;
+using Q42.HueApi.Streaming.Extensions;
+using Q42.HueApi.Streaming.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HueLightDJ.Effects.Layers
+{
+  [HueEffect(Name = "Colorloop Wheel Effect", HasColorPicker = false)]
+  public class ColorloopWheelEffect : IHueEffect
+  {
+    public async Task Start(EntertainmentLayer layer, Func<TimeSpan> waitTime, RGBColor? color, CancellationToken cancellationToken)
+    {
+      var r = new Random();
+      var orderedLayer = layer.OrderByDescending(x => x.LightLocation.Angle(0, 0) + 180);
+
+      while (!cancellationToken.IsCancellationRequested)
+      {
+        var randomColor = new RGBColor(r.NextDouble(), r.NextDouble(), r.NextDouble());
+
+        foreach (var light in orderedLayer)
+        {
+          var angle = light.LightLocation.Angle(0, 0) + 180;
+          var timeSpan = waitTime() / 360 * angle;
+          //Debug.WriteLine($"{light.Id} Angle {angle} and timespan {timeSpan.TotalMilliseconds}");
+          light.SetState(cancellationToken, randomColor, 1, timeSpan);
+        }
+
+        await Task.Delay(waitTime() * 1.2);
+      }
+
+    }
+  }
+}
